b9214080802ae34412244efd8e086f7b05edd860
Siddarth Sitaraman <36770264+sid2004@users.noreply.github.com>
2023-01-28 15:05:08 -0500

    Add files via upload

---
+++ locationVerifier.py
@@ -0,0 +1,9 @@
+import what3words
+
+geocoder = what3words.Geocoder("78P5ZDMW")
+def locationVerifier(newLong, newLat, placeLong, placeLat):
+    new3words = geocoder.convert_to_3wa(what3words.Coordinates(newLong, newLat))
+    place3words = geocoder.convert_to_3wa(what3words.Coordinates(placeLong, placeLat))
+    return new3words == place3words
+
+print(locationVerifier(51.521251, 0.203586, 51.521251, 0.203583))
