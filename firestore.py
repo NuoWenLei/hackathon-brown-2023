0ea899843c61602979e33c7161bee8e450d6efad
NuoWenLei <nuowen0612@gmail.com>
2023-01-28 14:50:36 -0500

    add basic apis

--- firestore.py
+++ firestore.py
@@ -1,4 +1,4 @@
-import firebase_admin
+import firebase_admin, uuid, datetime
 from firebase_admin import credentials
 from firebase_admin import firestore, storage
 
@@ -6,18 +6,36 @@ cred = credentials.Certificate('hackathon2023-brown-firebase-adminsdk-j9tvt-4d88
 
 # Application Default credentials are automatically created.
 app = firebase_admin.initialize_app(cred, {"storageBucket": "hackathon2023-brown.appspot.com"})
-
+db = firestore.client()
 bucket = storage.bucket()
+
+def get_unique_id():
+	doc_id = str(uuid.uuid4())
+	return doc_id + "-" + str(int(datetime.datetime.utcnow().timestamp()))
 
 def download_image_to_filename(fp):
 	blob = bucket.blob("something.jpg")
 	blob.download_to_filename(fp)
 
-db = firestore.client()
+def upload_image_bytearray_to_filename(bytearr, fp):
+	blob = bucket.blob(fp)
+	blob.upload_from_file(bytearr, content_type = "image/jpeg")
+	return fp
 
-ref = db.collection("photos").document("93WjlFDDGjBOtyKMenwU")
+def upload_place_tag(data):
+	col = db.collection("places")
+	dataDoc = get_unique_id()
+	col.document(dataDoc).create(data)
+	return dataDoc
 
-print(ref.get())
+def upload_photo_data(data):
+	col = db.collection("photos")
+	dataDoc = get_unique_id()
+	col.document(dataDoc).create(data)
+	return dataDoc
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from typing import Union
+import base64, re, io
+from PIL import Image
 
-from fastapi import FastAPI
+from image_processing import image_to_byte_array
+from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel
+from firestore import upload_image_bytearray_to_filename, get_unique_id, upload_photo_data, upload_place_tag
+
 
 # To run app
 # uvicorn main:app --reload
@@ -14,14 +20,41 @@ from fastapi import FastAPI
 # See interactive api at:
 # http://127.0.0.1:8000/docs
 
-
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/location")
+def add_pin(
+    lon: str = Form(...),
+    lat: str = Form(...)):
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    # TODO: maybe save as What3Words?
+    place_data = {
+        "lon": lon,
+        "lat": lat
+    }
+    placeRef = upload_place_tag(place_data)
+    return placeRef
+
+@app.post("/photo")
+def post_photo(
+    locationId: str = Form(...),
+    file_: str = Form(...)):
+
+    im = Image.open(io.BytesIO(base64.b64decode(re.sub('^data:image/.+;base64,', '', file_))))
+    im_resized_byte_array = image_to_byte_array(im)
+    filename = get_unique_id() + ".jpg"
+    fileRef = upload_image_bytearray_to_filename(im_resized_byte_array, filename)
+    photoMetadata = {
+        "location": locationId,
+        "photoRef": fileRef
+    }
+
+    dataRef = upload_photo_data(photoMetadata)
+    return dataRef
+
+
+    
