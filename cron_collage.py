1dd79f65e979e86ac93c19ee0bf73c4063066b81
NuoWenLei <nuowen0612@gmail.com>
2023-01-29 09:15:52 -0500

    Update cron_collage.py

--- cron_collage.py
+++ cron_collage.py
@@ -4,7 +4,7 @@ from collage_in_location import create_collage_in_location
 def main():
 	locs = get_all_locations()
 	for loc in locs:
-		if loc["num_images"] > 3:
+		if loc["num_images"] >= 3:
 			create_collage_in_location(loc["_id"])
 
 if __name__ == "__main__":
