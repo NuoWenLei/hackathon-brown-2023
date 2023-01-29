b4b31edafa3466e57aa2a770b70571611b4f96c0
Siddarth Sitaraman <36770264+sid2004@users.noreply.github.com>
2023-01-29 00:00:48 -0500

    Update locationVerifier.py

--- locationVerifier.py
+++ locationVerifier.py
@@ -1,12 +1,5 @@
-import what3words
-
-geocoder = what3words.Geocoder("78P5ZDMW")
 def locationVerifier(newLong, newLat, placeLong, placeLat):
-    new3words = geocoder.convert_to_3wa(what3words.Coordinates(newLat, newLong))
-    place3words = geocoder.convert_to_3wa(what3words.Coordinates(placeLat, placeLong))
-    return new3words == place3words
-
-def locationToW3W(lon, lat):
-    return geocoder.convert_to_3wa(what3words.Coordinates(lat, lon))
-
-print(locationVerifier(51.521251, 0.203586, 51.521251, 0.203583))
+    meterToCoord = 1 / 111000
+    lat_cond = placeLat - 10 * meterToCoord <= newLat <= 10 * meterToCoord + placeLat
+    long_cond = placeLong - 10 * meterToCoord <= newLong <= 10 * meterToCoord + placeLong
+    return (lat_cond and long_cond)
