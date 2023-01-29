42369fa4734f7082ea056afacb830abbf09bb8b9
Nuo Wen Lei <46566976+NuoWenLei@users.noreply.github.com>
2023-01-29 11:42:34 -0500

    Delete mongo.py

--- mongo.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-
-def createClient():
-	mongo_uri = "mongodb+srv://brown-hack-dev:THTmh2KowR1mOfBR@cluster0.jds65oj.mongodb.net/?retryWrites=true&w=majority"
-	client = pymongo.MongoClient(mongo_uri, tlsInsecure = True)
-	db = client.all_db
